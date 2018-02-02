aca75973e5dae670363cd85c7ee0c00a10606c0f
chenyou-fdu <chenyou.fdu@gmail.com>
2018-02-02 16:05:28 +0800

    working on crc32c

---
+++ util/src/main/java/org/chenyou/fuzzdb/util/UnsafeHelper.java
@@ -0,0 +1,33 @@
+package org.chenyou.fuzzdb.util;
+
+import sun.misc.Unsafe;
+import java.lang.reflect.Field;
+
+public class UnsafeHelper {
+
+    private final static Unsafe UNSAFE;
+    private final static Throwable UNSUPPORTED;
+    static {
+        Unsafe unsafe = null;
+        Throwable unsupported = null;
+        try {
+            Field theUnsafe = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            unsafe = (sun.misc.Unsafe) theUnsafe.get(null);
+        } catch (Throwable e) {
+            unsupported = e;
+        }
+
+        UNSAFE = unsafe;
+        UNSUPPORTED = unsupported;
+    }
+
+
+    public static Unsafe getUnsafe() throws UnsupportedOperationException {
+        Unsafe u = UNSAFE;
+        if (u == null) {
+            throw new UnsupportedOperationException(UNSUPPORTED);
+        }
+        return u;
+    }
+}
--- util/src/test/java/org/chenyou/fuzzdb/util/test/CRC32Test.java
+++ util/src/test/java/org/chenyou/fuzzdb/util/test/CRC32Test.java
@@ -1,9 +1,7 @@
 package org.chenyou.fuzzdb.util.test;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.Arrays;
-import org.chenyou.fuzzdb.util.FuzzCRC32;
+import org.chenyou.fuzzdb.util.FuzzCRC32C;
 public class CRC32Test {
 
     @Before
@@ -12,7 +10,10 @@ public class CRC32Test {
 
     @Test
     public void CRC32TestZero() {
-
+        FuzzCRC32C fuzzCRC32 = new FuzzCRC32C();
+        byte[] tmp = new byte[32];
+        fuzzCRC32.update(tmp, 0, 32);
+        long k = fuzzCRC32.getValue();
         return;
     }
 }
